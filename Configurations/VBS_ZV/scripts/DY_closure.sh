6590c8fc1949baa5a5be07b671fe2da7657d9834
matteo <matteo.presilla@pg.infn.it>
2022-12-23 21:43:35 +0100

    updating code

--- Configurations/VBS_ZV/scripts/DY_closure.sh
+++ Configurations/VBS_ZV/scripts/DY_closure.sh
@@ -9,51 +9,107 @@ mkdir $output_dir;
 # Closure test
 # run it like: sh ../../scripts/DY_closure.sh /eos/user/m/mpresill/CMS/VBS/VBS_ZV/dy_closure_test 14Dec2022_2017 configuration.py
 
-# 2017 resolved b-veto internal
-python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
-            -o $output_dir/2017/bins_norm_resolved_bveto_int.csv --vars DYfit_2D_bin_Resolved \
-            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
-            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_int  -c $3 
-
-## 2017 resolved b-veto internal
-python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
-            -o $output_dir/2017/bins_norm_resolved_bveto_ext.csv --vars DYfit_2D_bin_Resolved \
-            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
-            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_ext  -c $3 
-
-# 2017 resolved b-tag internal
-python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
-            -o $output_dir/2017/bins_norm_resolved_btag_int.csv --vars DYfit_2D_bin_Resolved \
-            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
-            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_int  -c $3 
-
-# 2017 resolved b-tag internal
-python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
-            -o $output_dir/2017/bins_norm_resolved_btag_ext.csv --vars DYfit_2D_bin_Resolved \
-            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
-            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_ext  -c $3 
+### 2017 resolved b-veto internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2017/bins_norm_resolved_bveto_int.csv --vars DYfit_2D_bin_Resolved \
+#            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_int  -c $3 
+#
+### 2017 resolved b-veto external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2017/bins_norm_resolved_bveto_ext.csv --vars DYfit_2D_bin_Resolved \
+#            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_ext  -c $3 
+#
+## 2017 resolved b-tag internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2017/bins_norm_resolved_btag_int.csv --vars DYfit_2D_bin_Resolved \
+#            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_int  -c $3 
+#
+## 2017 resolved b-tag external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2017/bins_norm_resolved_btag_ext.csv --vars DYfit_2D_bin_Resolved \
+#            -s DY_Resolved_2d_01  DY_Resolved_2d_02 DY_Resolved_2d_03 DY_Resolved_2d_04 DY_Resolved_2d_05 DY_Resolved_2d_06 DY_Resolved_2d_07 DY_Resolved_2d_08 DY_Resolved_2d_09 DY_Resolved_2d_10 DY_Resolved_2d_11 DY_Resolved_2d_12 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_ext  -c $3 
 
 
 # 2017 boosted b-veto internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
+#            -o $output_dir/2017/bins_norm_boosted_bveto_int.csv --vars DYfit_Z_bin_Boosted \
+#            -s DY_Boosted_Z_1 DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bVeto_int  -c $3 
+#
+### 2017 boosted b-veto external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
+#            -o $output_dir/2017/bins_norm_boosted_bveto_ext.csv --vars DYfit_Z_bin_Boosted \
+#            -s DY_Boosted_Z_1 DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5  \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bVeto_ext  -c $3 
+#
+## 2017 boosted b-tag internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
+#            -o $output_dir/2017/bins_norm_boosted_btag_int.csv --vars DYfit_Z_bin_Boosted \
+#            -s DY_Boosted_Z_1  DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bTag_int  -c $3 
+#
+## 2017 boosted b-tag external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
+#            -o $output_dir/2017/bins_norm_boosted_btag_ext.csv --vars DYfit_Z_bin_Boosted \
+#            -s DY_Boosted_Z_1  DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bTag_ext  -c $3 
+
+## 2016 resolved b-veto internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2016/bins_norm_resolved_bveto_int.csv --vars DYfit_Z_bin \
+#            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_int  -c $3 
+#
+### 2016 resolved b-veto external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2016/bins_norm_resolved_bveto_ext.csv --vars DYfit_Z_bin \
+#            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto_ext  -c $3 
+#
+## 2016 resolved b-tag internal
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2016/bins_norm_resolved_btag_int.csv --vars DYfit_Z_bin \
+#            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_int  -c $3 
+#
+## 2016 resolved b-tag external
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2016/bins_norm_resolved_btag_ext.csv --vars DYfit_Z_bin \
+#            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bTag_ext  -c $3 
+#
+## 2016 resolved b-veto
+#python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_resolved.root \
+#            -o $output_dir/2016/bins_norm_resolved_bveto.csv --vars DYfit_Z_bin \
+#            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5 \
+#            --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Resolved_DYcr_bVeto  -c $3 
+
+
+
+# 2016 boosted b-veto internal
 python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
-            -o $output_dir/2017/bins_norm_boosted_bveto_int.csv --vars DYfit_Z_bin_Boosted \
-            -s DY_Boosted_Z_1 DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+            -o $output_dir/2016/bins_norm_boosted_bveto_int.csv --vars DYfit_Z_bin \
+            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
             --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bVeto_int  -c $3 
 
-## 2017 boosted b-veto internal
+# 2016 boosted b-veto external
 python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
-            -o $output_dir/2017/bins_norm_boosted_bveto_ext.csv --vars DYfit_Z_bin_Boosted \
-            -s DY_Boosted_Z_1 DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5  \
+            -o $output_dir/2016/bins_norm_boosted_bveto_ext.csv --vars DYfit_Z_bin \
+            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5   \
             --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bVeto_ext  -c $3 
-
-# 2017 boosted b-tag internal
+            
+# 2016 boosted b-tag internal
 python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
-            -o $output_dir/2017/bins_norm_boosted_btag_int.csv --vars DYfit_Z_bin_Boosted \
-            -s DY_Boosted_Z_1  DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+            -o $output_dir/2016/bins_norm_boosted_btag_int.csv --vars DYfit_Z_bin \
+            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
             --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bTag_int  -c $3 
 
-# 2017 boosted b-tag internal
+# 2016 boosted b-tag external
 python ../../scripts/bins_norm_reweights_withsyst.py -i /eos/user/m/mpresill/CMS/VBS/VBS_ZV/histograms/rootFile_$name/plots_VBS_ZV_${name}_boosted.root \
-            -o $output_dir/2017/bins_norm_boosted_btag_ext.csv --vars DYfit_Z_bin_Boosted \
-            -s DY_Boosted_Z_1  DY_Boosted_Z_2 DY_Boosted_Z_3 DY_Boosted_Z_4 DY_Boosted_Z_5 \
+            -o $output_dir/2016/bins_norm_boosted_btag_ext.csv --vars DYfit_Z_bin \
+            -s DY_bin1 DY_bin2 DY_bin3 DY_bin4 DY_bin5  \
             --other-samples tZq VBS_VV_QCD WJets WW ggWW VVV Fake sm_dipole top Vg VgS VBF-V VZ --cut Boosted_DYcr_bTag_ext  -c $3 
