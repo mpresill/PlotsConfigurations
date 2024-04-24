b236edf7a83849602eb5f1467c1118b330c493b0
matteo <matteo.presilla@pg.infn.it>
2024-04-24 17:48:08 +0200

    my eft dictionary

---
+++ Configurations/VBS_ZV/EFT/ReweightFactory/EFT_dim8_dictionary_v2.py
@@ -0,0 +1,102 @@
+operators = {
+    'cT4': {
+        'sm': '( LHEReweightingWeight[1174] )',
+        'LinReweight': '( 0.5* (1/(2)) * ( LHEReweightingWeight[1194] - LHEReweightingWeight[1154] ))',
+        'quadReweight': '( 0.5* (1/(2)) * (1/(2)) * ( LHEReweightingWeight[1194] + LHEReweightingWeight[1154] - 2 * LHEReweightingWeight[1174]))',
+    },
+    'cT5': {
+        'sm': '( LHEReweightingWeight[1255] )',
+        'LinReweight': '( 0.5* (1/(1)) * ( LHEReweightingWeight[1260] - LHEReweightingWeight[1250] ))',
+        'quadReweight': '( 0.5* (1/(1)) * (1/(1)) * ( LHEReweightingWeight[1260] + LHEReweightingWeight[1250] - 2 * LHEReweightingWeight[1255]))',
+    },
+    'cT6': {
+        'sm': '( LHEReweightingWeight[1336] )',
+        'LinReweight': '( 0.5* (1/(1)) * ( LHEReweightingWeight[1341] - LHEReweightingWeight[1331] ))',
+        'quadReweight': '( 0.5* (1/(1)) * (1/(1)) * ( LHEReweightingWeight[1341] + LHEReweightingWeight[1331] - 2 * LHEReweightingWeight[1336]))',
+    },
+    'cT7': {
+        'sm': '( LHEReweightingWeight[1417] )',
+        'LinReweight': '( 0.5* (1/(2)) * ( LHEReweightingWeight[1422] - LHEReweightingWeight[1412] ))',
+        'quadReweight': '( 0.5* (1/(2)) * (1/(2)) * ( LHEReweightingWeight[1422] + LHEReweightingWeight[1412] - 2 * LHEReweightingWeight[1417]))',
+    },
+    'cT0': {
+        'sm': '( LHEReweightingWeight[850] )',
+        'LinReweight': '( 0.5* (1/(0.5)) * ( LHEReweightingWeight[860] - LHEReweightingWeight[840] ))',
+        'quadReweight': '( 0.5* (1/(0.5)) * (1/(0.5)) * ( LHEReweightingWeight[860] + LHEReweightingWeight[840] - 2 * LHEReweightingWeight[850]))',
+    },
+    'cT1': {
+        'sm': '( LHEReweightingWeight[931] )',
+        'LinReweight': '( 0.5* (1/(0.5)) * ( LHEReweightingWeight[941] - LHEReweightingWeight[921] ))',
+        'quadReweight': '( 0.5* (1/(0.5)) * (1/(0.5)) * ( LHEReweightingWeight[941] + LHEReweightingWeight[921] - 2 * LHEReweightingWeight[931]))',
+    },
+    'cT2': {
+        'sm': '( LHEReweightingWeight[1012] )',
+        'LinReweight': '( 0.5* (1/(0.5)) * ( LHEReweightingWeight[1017] - LHEReweightingWeight[1007] ))',
+        'quadReweight': '( 0.5* (1/(0.5)) * (1/(0.5)) * ( LHEReweightingWeight[1017] + LHEReweightingWeight[1007] - 2 * LHEReweightingWeight[1012]))',
+    },
+    'cT3': {
+        'sm': '( LHEReweightingWeight[1093] )',
+        'LinReweight': '( 0.5* (1/(0.5)) * ( LHEReweightingWeight[1098] - LHEReweightingWeight[1088] ))',
+        'quadReweight': '( 0.5* (1/(0.5)) * (1/(0.5)) * ( LHEReweightingWeight[1098] + LHEReweightingWeight[1088] - 2 * LHEReweightingWeight[1093]))',
+    },
+    'cS1': {
+        'sm': '( LHEReweightingWeight[121] )',
+        'LinReweight': '( 0.5* (1/(5.25)) * ( LHEReweightingWeight[128] - LHEReweightingWeight[114] ))',
+        'quadReweight': '( 0.5* (1/(5.25)) * (1/(5.25)) * ( LHEReweightingWeight[128] + LHEReweightingWeight[114] - 2 * LHEReweightingWeight[121]))',
+    },
+    'cT8': {
+        'sm': '( LHEReweightingWeight[1498] )',
+        'LinReweight': '( 0.5* (1/(0.5)) * ( LHEReweightingWeight[1499] - LHEReweightingWeight[1497] ))',
+        'quadReweight': '( 0.5* (1/(0.5)) * (1/(0.5)) * ( LHEReweightingWeight[1499] + LHEReweightingWeight[1497] - 2 * LHEReweightingWeight[1498]))',
+    },
+    'cT9': {
+        'sm': '( LHEReweightingWeight[1579] )',
+        'LinReweight': '( 0.5* (1/(1.5)) * ( LHEReweightingWeight[1582] - LHEReweightingWeight[1576] ))',
+        'quadReweight': '( 0.5* (1/(1.5)) * (1/(1.5)) * ( LHEReweightingWeight[1582] + LHEReweightingWeight[1576] - 2 * LHEReweightingWeight[1579]))',
+    },
+    'cS0': {
+        'sm': '( LHEReweightingWeight[40] )',
+        'LinReweight': '( 0.5* (1/(5.25)) * ( LHEReweightingWeight[47] - LHEReweightingWeight[33] ))',
+        'quadReweight': '( 0.5* (1/(5.25)) * (1/(5.25)) * ( LHEReweightingWeight[47] + LHEReweightingWeight[33] - 2 * LHEReweightingWeight[40]))',
+    },
+    'cM7': {
+        'sm': '( LHEReweightingWeight[769] )',
+        'LinReweight': '( 0.5* (1/(10)) * ( LHEReweightingWeight[774] - LHEReweightingWeight[764] ))',
+        'quadReweight': '( 0.5* (1/(10)) * (1/(10)) * ( LHEReweightingWeight[774] + LHEReweightingWeight[764] - 2 * LHEReweightingWeight[769]))',
+    },
+    'cS2': {
+        'sm': '( LHEReweightingWeight[202] )',
+        'LinReweight': '( 0.5* (1/(5.25)) * ( LHEReweightingWeight[209] - LHEReweightingWeight[195] ))',
+        'quadReweight': '( 0.5* (1/(5.25)) * (1/(5.25)) * ( LHEReweightingWeight[209] + LHEReweightingWeight[195] - 2 * LHEReweightingWeight[202]))',
+    },
+    'cM5': {
+        'sm': '( LHEReweightingWeight[688] )',
+        'LinReweight': '( 0.5* (1/(4)) * ( LHEReweightingWeight[689] - LHEReweightingWeight[687] ))',
+        'quadReweight': '( 0.5* (1/(4)) * (1/(4)) * ( LHEReweightingWeight[689] + LHEReweightingWeight[687] - 2 * LHEReweightingWeight[688]))',
+    },
+    'cM4': {
+        'sm': '( LHEReweightingWeight[607] )',
+        'LinReweight': '( 0.5* (1/(4)) * ( LHEReweightingWeight[609] - LHEReweightingWeight[605] ))',
+        'quadReweight': '( 0.5* (1/(4)) * (1/(4)) * ( LHEReweightingWeight[609] + LHEReweightingWeight[605] - 2 * LHEReweightingWeight[607]))',
+    },
+    'cM3': {
+        'sm': '( LHEReweightingWeight[526] )',
+        'LinReweight': '( 0.5* (1/(6)) * ( LHEReweightingWeight[529] - LHEReweightingWeight[523] ))',
+        'quadReweight': '( 0.5* (1/(6)) * (1/(6)) * ( LHEReweightingWeight[529] + LHEReweightingWeight[523] - 2 * LHEReweightingWeight[526]))',
+    },
+    'cM2': {
+        'sm': '( LHEReweightingWeight[445] )',
+        'LinReweight': '( 0.5* (1/(3)) * ( LHEReweightingWeight[447] - LHEReweightingWeight[443] ))',
+        'quadReweight': '( 0.5* (1/(3)) * (1/(3)) * ( LHEReweightingWeight[447] + LHEReweightingWeight[443] - 2 * LHEReweightingWeight[445]))',
+    },
+    'cM1': {
+        'sm': '( LHEReweightingWeight[364] )',
+        'LinReweight': '( 0.5* (1/(4.9)) * ( LHEReweightingWeight[371] - LHEReweightingWeight[357] ))',
+        'quadReweight': '( 0.5* (1/(4.9)) * (1/(4.9)) * ( LHEReweightingWeight[371] + LHEReweightingWeight[357] - 2 * LHEReweightingWeight[364]))',
+    },
+    'cM0': {
+        'sm': '( LHEReweightingWeight[283] )',
+        'LinReweight': '( 0.5* (1/(1.8)) * ( LHEReweightingWeight[285] - LHEReweightingWeight[281] ))',
+        'quadReweight': '( 0.5* (1/(1.8)) * (1/(1.8)) * ( LHEReweightingWeight[285] + LHEReweightingWeight[281] - 2 * LHEReweightingWeight[283]))',
+    },
+}
